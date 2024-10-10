77091ff35cd7494547b08b1022e60f053cc2f8c5
b33198 <b33198@sfc.potteries.ac.uk>
2024-10-10 15:37:22 +0100

    git commit

--- Main.py
+++ Main.py
@@ -33,6 +33,57 @@ class App():
         self.homeScreen()
         return
 
+def homeScreen(self):
+    #creates window using window class from ui window module
+    #passes screen title and width and height of window
+    win1 = UI.Window('Home',500,350,self.formColour,self.fontColour,self.font)
+
+    #calls centrewindow method from window class to centre the window within the users screen
+    win1.centreWindow()
+
+    #takes 'header' and 'body' attributes from 'win1' object and stores them in variables 'headerFrame' and 'bodyFrame'
+    headerFrame = win1.getHeader()
+    bodyFrame = win1.getBody()
+
+    win1.showTitle(160)
+
+    #craetes title labelfor window 'win1'
+    titleLabel = win1.customLabel('header','Home Page',10)
+    titleLabel.grid(column=1,row=1)
+    titleLabel['fg']= '#747373'
+
+    courseLabel = win1.customLabel('body', 'To view and edit courses:',10)
+    courseLabel.grid(row=0,column=1, pady=10)
+
+    #craeyes button to navigate to screen displaying the user's courses
+    courseButton= Button(bodyFrame,text="Courses", command= lambda:[win1.destroyWindow(),self.courseScreen()])
+    courseButton.grid(row=1,column=1)
+    courseButton['bg']=win1.getColour()
+    courseButton['fg']= win1.getFontColour()
+    courseButton['font']= (self.font,10)
+
+    taskLabel = win1.customLabel('body', 'To view and edit tasks:',10)
+    taskLabel.grid(row=2,column=1, pady=10)
+
+    #creates button to navigate to user's tasks
+    taskButton= Button(bodyFrame,text="Tasks", command= lambda:[win1.destroyWindow(),self.taskScreen()])
+    taskButton.grid(row=3,column=1,padx=10)
+    taskButton['bg']=win1.getColour()
+    taskButton['fg']= win1.getFontColour()
+    taskButton['font']= (self.font,10)
+
+    settingsLabel = win1.customLabel('body', 'To view and edit settings:',10)
+    settingsLabel.grid(row=4,column=1, pady=10)
+    settingsButton=Button(bodyFrame,text = 'Settings', command= lambda:[win1.destroyWindow(),self.settingsScreen()])
+    settingsButton.grid(row=5,column=1,)
+    settingsButton['bg']=win1.getColour()
+    settingsButton['fg']= win1.getFontColour()
+    settingsButton['font']= (self.font,10)
+
+    win1.mainloop()
+    return
+
+
 if __name__ == "__main__":
     print("running main.py")
 
